8050b7db4d93fbcb60b2eec1291490f2af0d6b0e
Paul Kegel <paul@bemorecompany.com>
2020-09-13 22:45:29 +0200

    Find the middle

--- src/drawflow.js
+++ src/drawflow.js
@@ -583,7 +583,7 @@ export default class Drawflow {
           var hx1 = line_x + Math.abs(x - line_x) * curvature;
           var hx2 = x - Math.abs(x - line_x) * curvature;
         }
-        return ' M '+ line_x +' '+ line_y +' C '+ hx1 +' '+ line_y +' '+ hx2 +' ' + y +' ' + x +'  ' + y;
+        return ' M '+ line_x +' '+ line_y +' C '+ hx1 +' '+ line_y +' '+ hx2 +' ' + y +' ' + x +' ' + y;
 
         break
       case 'close':
@@ -667,11 +667,32 @@ export default class Drawflow {
     var lineCurve = this.createCurvature(line_x, line_y, x, y, curvature, 'openclose');
     path.setAttributeNS(null, 'd', lineCurve);
 
-    var xc = ((line_x + x) / 2);
-    var yc = ((line_y + y) / 2);
+    function lerp(A, B, t) {
+      // A and B are arrays where the first element is the x 
+      // and the second element is the y coordinate of the point
+      // if(t == .5) the function returns a point in the center of the line AB
+      // t is always a number between 0 and 1
+      // 0 <= t <= 1
+      return [
+        (B[0] - A[0]) * t + A[0], // the x coordinate
+        (B[1] - A[1]) * t + A[1]  // the y coordinate
+      ];
+    }
 
-    image.setAttribute('x',xc);
-    image.setAttribute('y',yc);
+    var hx1 = line_x + Math.abs(x - line_x) * curvature;
+    var hx2 = x - Math.abs(x - line_x) * curvature;
+
+    let t = .5;
+    let helperPoints = [];
+    helperPoints[0] = lerp([line_x, line_y], [hx1, line_y], t);
+    helperPoints[1] = lerp([hx1, line_y], [hx2, y], t);
+    helperPoints[2] = lerp([hx2, y], [x, y], t);
+    helperPoints[3] = lerp(helperPoints[0], helperPoints[1], t);
+    helperPoints[4] = lerp(helperPoints[1], helperPoints[2], t);
+    helperPoints[5] = lerp(helperPoints[3], helperPoints[4], t);
+
+    image.setAttribute('x',helperPoints[5][0]);
+    image.setAttribute('y',helperPoints[5][1]);
 
   }
 
